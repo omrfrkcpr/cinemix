d5fa411e56e89475b02b0673b95c58c081e83e6d
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-04-05 18:00:47 +0200

    feat: implement authentication pages (login/signup/reset/profile)

---
+++ src/pages/Reset.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuthContext } from "../context/AuthContext";
+
+const Reset = () => {
+  const [email, setEmail] = useState("");
+
+  const { resetPassword } = useAuthContext();
+  const navigate = useNavigate();
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      await resetPassword(email);
+      navigate("/login");
+      setTimeout(() => {
+        setEmail("");
+      }, 1000);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <>
+      <div className="w-full h-screen">
+        <img
+          className="hidden sm:block absolute w-full h-full object-cover"
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/77d35039-751f-4c3e-9c8d-1240c1ca6188/cf244808-d722-428f-80a9-052acdf158ec/IN-en-20231106-popsignuptwoweeks-perspective_alpha_website_small.jpg"
+          alt="///"
+        />
+        <div className="bg-black/70 fixed top-0 left-0 w-full h-screen" />
+
+        <div className="fixed w-full px-4 py-24 z-20">
+          <div className="max-w-[450px] h-[600px] mx-auto bg-black/80 rounded-lg">
+            <div className="max-w-[320px] mx-auto py-16">
+              <h1 className="text-3xl font-nsans-bold">Login</h1>
+
+              <form
+                onSubmit={handleFormSubmit}
+                className="w-full flex flex-col py-4"
+              >
+                <input
+                  className="p-3 my-2 bg-gray-700 rounded"
+                  type="email"
+                  placeholder="email"
+                  autoComplete="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <button className="bg-red-600 py-3 my-6 rounded font-nsans-bold">
+                  Login
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Reset;
