4dabd3a41b1512727e91e7c87db7b21e2c80ef30
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-04-05 17:48:57 +0200

    feat: implement authentication provider + add toastify messages

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,7 +1,111 @@
-import React from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { toastErrorNotify, toastSuccessNotify } from "../helpers/toastNotify";
+import { useNavigate } from "react-router-dom";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+  updateProfile,
+  signInWithPopup,
+  sendPasswordResetEmail,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import { auth, db } from "../auth/firebase";
+import { doc, setDoc } from "firebase/firestore";
 
-const AuthContext = () => {
-  return <div>AuthContext</div>;
+const AuthContext = createContext();
+
+// context provider
+
+const AuthContextProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState({});
+  const navigate = useNavigate();
+
+  const userObserver = () => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // const { email, displayName, photoURL } = user;
+        setCurrentUser(user);
+      }
+    });
+  };
+
+  useEffect(() => {
+    userObserver();
+  }, []);
+
+  const signup = async (email, password, displayName) => {
+    try {
+      //? yeni bir kullanıcı oluşturmak için kullanılan firebase metodu
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      //? kullanıcı profilini güncellemek için kullanılan firebase metodu
+      await updateProfile(auth.currentUser, {
+        displayName: displayName,
+      });
+      setDoc(doc(db, "users", email)),
+        {
+          favShows: [],
+        };
+      navigate("/login");
+      toastSuccessNotify("You are successfully registered!");
+      console.log(userCredential);
+    } catch (error) {
+      toastErrorNotify(error.message);
+    }
+  };
+
+  const login = async (email, password) => {
+    await signInWithEmailAndPassword(auth, email, password);
+    toastSuccessNotify("Logged in!");
+  };
+
+  const signGoogleProvider = async () => {
+    try {
+      const provider = new GoogleAuthProvider();
+      await signInWithPopup(auth, provider);
+      navigate("/");
+      toastSuccessNotify("Logged in!");
+    } catch (err) {
+      toastErrorNotify(err.message);
+    }
+  };
+
+  const logout = () => {
+    signOut(auth);
+    toastSuccessNotify("Logged out!");
+  };
+
+  const resetPassword = async (email) => {
+    try {
+      await sendPasswordResetEmail(auth, email);
+      toastSuccessNotify("Password reset email sent successfully.");
+    } catch (error) {
+      toastErrorNotify("Error sending password reset email:", error.message);
+    } finally {
+      navigate("/login");
+    }
+  };
+
+  const values = {
+    currentUser,
+    signup,
+    login,
+    logout,
+    resetPassword,
+    signGoogleProvider,
+  };
+
+  return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>;
 };
 
-export default AuthContext;
+// consumer with custom hook
+export const useAuthContext = () => {
+  return useContext(AuthContext);
+};
+
+export default AuthContextProvider;
--- src/helpers/toastNotify.js
+++ src/helpers/toastNotify.js
@@ -1,7 +1,34 @@
-import React from "react";
+import { toast } from "react-toastify";
 
-const toastNotify = () => {
-  return <div>toastNotify</div>;
+export const toastSuccessNotify = (msg) =>
+  toast.success(msg, {
+    position: "top-right",
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "colored",
+  });
+
+export const toastWarnNotify = (msg) => {
+  toast.warn(msg, {
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+  });
 };
-
-export default toastNotify;
+export const toastErrorNotify = (msg) => {
+  toast.error(msg, {
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+  });
+};
