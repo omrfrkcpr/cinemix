b59a6d3244521a56d9c29d23b8141f687bb19694
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-04-05 18:04:25 +0200

    feat: remove eslint package + implement NotFound page and ErrorPage component

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Navigate } from "react-router-dom";
 
 const ErrorPage = () => {
-  return <div>ErrorPage</div>;
+  const [count, setCount] = useState(10);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCount((prev) => (prev === 0 ? prev : prev - 1)); // thanks to prev, we can always catch updated state
+    }, 1000);
+    return () => clearInterval(timer);
+  }, []);
+  if (count === 0) {
+    return <Navigate to="/" replace />;
+  }
+  return (
+    <main>
+      <div className="max-w-screen-xl mx-auto px-4 flex items-center justify-start h-screen md:px-8">
+        <div className="max-w-lg mx-auto space-y-3 text-center">
+          <h3 className="text-gray-800 dark:text-neutral-200 text-4xl font-semibold sm:text-5xl">
+            Someting went wrong!
+          </h3>
+          <p className="text-gray-600 dark:text-gray-200">
+            Within <strong>{count}</strong> seconds, you will be redirected to
+            the home page!
+          </p>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default ErrorPage;
